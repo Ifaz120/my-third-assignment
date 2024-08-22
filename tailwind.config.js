8d391b3b41d37f9849d51b2fe966e1a17ea4ace7
Iftekhar-Ahmed-Ifaz <iftekhariftekhar099@gmail.com>
2024-08-22 14:20:17 +0600

    about us done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        colors: {
+          'neo-gray': '#494949',
+        },
+      },
     },
     plugins: [],
   }
